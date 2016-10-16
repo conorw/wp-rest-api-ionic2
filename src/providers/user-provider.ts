14bcc9dca3fe6dd703547b78eda7bd98c51c42e2
CONOR WOODS <CONOR WOODS>
2016-10-16 20:53:48 +0100

    add proxy to allow login

--- src/providers/user-provider.ts
+++ src/providers/user-provider.ts
@@ -2,20 +2,28 @@ import { Injectable } from '@angular/core';
 
 import { Events } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class UserProvider {
     stor: Storage;
 
-    constructor(public events: Events) { 
+    constructor(public events: Events, private http: Http) {
         this.stor = new Storage();
-
     }
     AUTHTOKEN: string = "authtoken";
     login(username, password) {
+        let data = { username: username, password: password };
+
         this.stor.remove(this.AUTHTOKEN);
-        this.stor.set(this.AUTHTOKEN,username);
-        this.events.publish('user:login');
+        // the important bit, contact the end point and ask for a token
+        this.http.post('/server/wp-json/jwt-auth/v1/token', data)
+            .subscribe(data => {
+                this.stor.set(this.AUTHTOKEN, data);
+                this.events.publish('user:login');
+            },
+            err => console.log(err)
+            );
     }
 
 
