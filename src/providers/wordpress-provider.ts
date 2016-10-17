59008b08cbf21477efe0e8ed0737a180a0e1332f
Conor Woods <conorw@gmail.com>
2016-10-17 13:19:41 +0100

    refactor wordpress provide

--- src/providers/wordpress-provider.ts
+++ src/providers/wordpress-provider.ts
@@ -11,27 +11,6 @@ export class WordPressProvider {
 
     }
 
-    createReport(username, password) {
-        let data = { username: username, password: password };
-
-        this.stor.remove(this.AUTHTOKEN);
-        // the important bit, contact the end point and ask for a token
-        this.http.post('/server/wp-json/jwt-auth/v1/token', data)
-            .subscribe(data => {
-                this.stor.set(this.AUTHTOKEN, data);
-                this.events.publish('user:login');
-            },
-            err => console.log(err)
-            );
-    }
-    logout() {
-        this.stor.remove(this.AUTHTOKEN);
-        this.events.publish('user:logout');
-    }
-    checkedLoggedInStatus() {
-        this.stor.get(this.AUTHTOKEN).then(output=>{
-            if(output){
-                this.events.publish('user:login');
-            }});
+    createReport(score: string, report: string) {
     }
 }
