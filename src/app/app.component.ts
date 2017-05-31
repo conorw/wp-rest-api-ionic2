b659912a637788508dbba8adb11792b37fd85024
conorw <conorw@gmail.com>
2017-05-31 13:43:15 +0100

    reformat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,15 @@ import { UserProvider } from '../providers/user-provider';
 export class MyApp {
   // the default root page is Login
   rootPage: any = Login;
-  constructor(
-    private events: Events,
-    private userData: UserProvider
-  ) {
-    // start listening to login and log out events
-    this.listenToLoginEvents();
-    // check to see if user has logged in before
-    // if they have there will be a logged in event fired
-    userData.checkedLoggedInStatus();
-  }
 
-  listenToLoginEvents() {
+  constructor(private events: Events, private userData: UserProvider) {
+    // start listening to login and log out events
     // if the user is logged in then navigate to the Report page
     this.events.subscribe('user:login', () => this.rootPage = Report);
     this.events.subscribe('user:logout', () => this.rootPage = Login);
+
+    // check to see if user has logged in before
+    // if they have there will be a logged in event fired
+    userData.checkedLoggedInStatus();
   }
 }
