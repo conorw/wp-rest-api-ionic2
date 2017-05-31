7bf64c55561621c535c3f015ae367396882e3df0
conorw <conorw@gmail.com>
2017-05-31 13:38:01 +0100

    move http client declaration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { Login } from '../pages/login/login';
 import { Report } from '../pages/report/report';
 import { UserProvider } from '../providers/user-provider';
 import { WordPressProvider } from '../providers/word-press-provider';
-import { HttpClient } from '../providers/http-client';
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`,
-  providers: [UserProvider, WordPressProvider, HttpClient]
+  providers: [UserProvider, WordPressProvider]
 })
 export class MyApp {
   // the default root page is Login
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { IonicStorageModule } from '@ionic/storage';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpClient } from '../providers/http-client';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    HttpClient,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
